856276cda10117c624d7ba2b40cf52eaf529028b
Mark Eaton <mark.eaton@kbcc.cuny.edu>
2017-11-04 22:47:53 -0400

    initial commit

---
+++ parse-tweets.py
@@ -0,0 +1,20 @@
+import argparse
+import csv
+import sys
+
+parser = argparse.ArgumentParser(description='Process TCAT archive '
+                                             'into a list of tweets')
+parser.add_argument('-f', metavar='FILENAME', help='specify filename')
+if len(sys.argv) == 1:
+    parser.print_usage()
+    sys.exit(1)
+args = parser.parse_args()
+
+with open(args.f) as f:
+    d = f.readlines()
+
+c = csv.DictReader(d)
+for row in c:
+    print(row['from_user_name'] + ' (' + row['from_user_realname'] + ')')
+    print(row['created_at'])
+    print(row['text'] + '\n')
